d311583b48af5518a61f9b84e603f80dc08d35f1
Branden Rodgers <branden@rodgersworld.com>
2025-07-14 19:41:09 -0400

    less user config needed

--- scripts/action-utils.sh
+++ scripts/action-utils.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+setup_hubspot_cli() {
+  # Check if hs CLI is already installed with correct version
+  if ! command -v hs &> /dev/null || [[ "$HUBSPOT_CLI_VERSION" != "latest" && ! "$(hs --version)" =~ "$HUBSPOT_CLI_VERSION" ]]; then
+    echo "Installing HubSpot CLI version $HUBSPOT_CLI_VERSION"
+    npm install -g "@hubspot/cli@$HUBSPOT_CLI_VERSION"
+  else
+    echo "HubSpot CLI is already installed with correct version"
+  fi
+}
 
 validate_account_and_personal_access_key() {
   if [ -z "$HUBSPOT_ACCOUNT_ID" ]; then
